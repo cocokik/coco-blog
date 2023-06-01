31a6d7bf184824ab10eef6d41143d4880c3d94f6
coco-mac <anrqhwl@gmail.com>
2023-06-01 11:19:28 +0900

    add2

--- src/main/java/com/cocokik/blog/config/SecurityConfig.java
+++ src/main/java/com/cocokik/blog/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.cocokik.blog.config;
 
 import com.cocokik.blog.config.auth.PrincipalDetailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -15,16 +16,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration //bean 등록
 @EnableWebSecurity //시큐리티에 Filter 추가
 @EnableMethodSecurity(prePostEnabled = false) //특정 주소로 접근시 권한 미리 체크하겠다
+@RequiredArgsConstructor
 public class SecurityConfig  {
     @Autowired
     private PrincipalDetailService principalDetailService;
+
+    @Autowired
+    private CorsFilter corsFilter;
     @Bean
     public BCryptPasswordEncoder encodedPWD(){
         return new BCryptPasswordEncoder();
@@ -54,18 +61,18 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //session don't not use
+        http.addFilter(corsFilter);
+        http.formLogin().disable();
+        http.httpBasic().disable();
         http.authorizeHttpRequests()
-                .requestMatchers("/", "").permitAll()
-                .requestMatchers("/auth/**", "/dummy/**", "/user/aboutForm").permitAll()
-                .requestMatchers("/js/**", "/image/**", "/css/**").permitAll()
-                .requestMatchers("/aa/**", "/WEB-INF/**").permitAll() //JSP 경로도 추가해야함..
-                .anyRequest().permitAll()
-                .and().formLogin().loginPage("/auth/loginForm")
-                .loginProcessingUrl("/auth/loginProc") //spring security 가 해당 url로 오는 요청을 가로챈다.
-                .defaultSuccessUrl("/");
-        http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(principalDetailService)
-                .passwordEncoder(encodedPWD());
+                .requestMatchers("/api/userUpdate").authenticated()
+                .requestMatchers("updateForm").authenticated()
+                .requestMatchers("/saveForm").authenticated()
+                .anyRequest().permitAll();
+//        http.getSharedObject(AuthenticationManagerBuilder.class)
+//                .userDetailsService(principalDetailService)
+//                .passwordEncoder(encodedPWD());
         return http.build();
     }
 }
--- src/main/java/com/cocokik/blog/controller/RestApiController.java
+++ src/main/java/com/cocokik/blog/controller/RestApiController.java
@@ -1,2 +1,15 @@
-package com.cocokik.blog.controller;public class RestApiController {
+package com.cocokik.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApiController {
+    @GetMapping("/home")
+    public String home(){
+        return "<H1>Home</H1>";
+    }
+
 }
+
+
